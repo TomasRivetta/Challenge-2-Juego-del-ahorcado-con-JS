4e5304df852d23daf639b3d5860f964c693a3321
TomasRivetta <totorivetta@gmail.com>
2022-09-07 19:03:41 -0300

    arreglando error de dibujar letra

--- JavaScript/coding.js
+++ JavaScript/coding.js
@@ -1,5 +1,7 @@
 //FUNCION OCULTAR Y MOSTRAR
 
+
+
 function mostrar(){
     document.getElementById("mostrarJuego").style.display = "block";
 }
@@ -19,6 +21,7 @@ const palabras = ["HTML", "CSS", "PYTHON", "SQL" , "REDUX" , "REACTJS" , "NODEJS
 function crearPalabraSecreta(){
     palabraAleatoria = palabras[Math.floor(Math.random() * palabras.length)]
 
+
     //LLAMADO A LA TABLA VACIA
     ctx.reset()
 
@@ -26,8 +29,6 @@ function crearPalabraSecreta(){
     guionesPalabra(palabraAleatoria)
 
 
-    //VERIFICAR LETRA CORRECTA
-    dibujarLetra(palabraAleatoria)
 
     //MUESTRA EN CONSOLA PRUEBA
     console.log(palabraAleatoria)
@@ -62,45 +63,42 @@ function guionesPalabra(palabraAleatoria){
 }
 
 
-//ACA DEJE 06/09
-
-
-
 //FUNCION: VERIFICAR SI LA TECLA PRESIONADA ES UNA LETRA
 //- Debe funcionar solo con letras mayúsculas;
 //- No deben ser utilizadas letras con acentos ni caracteres especiales;
 //console.log(event)
 //console.log(event.key)
 
-function verificarTeclaLetra(){
-    document.addEventListener("keydown",verificacion)
-}
+document.addEventListener("keydown",verificacion)
 
 function verificacion(event){
     const teclaPresionada = event.key
-
-    console.log(teclaPresionada)
 
     key = event.KeyCode || event.which;
     tecla = String.fromCharCode(key).toString();
     letrasAceptadas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     if (letrasAceptadas.indexOf(tecla) == -1){
-        alert("Ingrese solo letras mayúsculas");
+        alert("ACTIVE EL BLOQ MAYUS");
     }
     else
     {
-        dibujarLetra(palabraAleatoria,teclaPresionada)
+        dibujarLetra(teclaPresionada)
     }
 
 }
 
 //FUNCION: DIBUJAR LETRA CORRECTA
 
-function dibujarLetra(palabraAleatoria,teclaPresionada){
+
+function dibujarLetra(teclaPresionada){
 
 
     let palabra = palabraAleatoria
     let tecla = teclaPresionada;
+    console.log(tecla)
+
+
+    //
     
     let movimiento = 13 * 2
     for(i = 0; i < palabra.length; i++)
@@ -112,24 +110,14 @@ function dibujarLetra(palabraAleatoria,teclaPresionada){
             ctx.fillStyle = "white";
             ctx.fillText(tecla,-96.3,120);
             movimiento++
+            return
         }
     }
-    
-    //dibujarLetraIncorrecta(teclaPresionada,palabra)
 
-    /*
-    let palabra = palabraAleatoria
-    let tecla = teclaPresionada
-    for(i = 0; i < palabra.length; i++)
-    {
-        if(tecla == palabra[i])
-        {
-            ctx.font="10pt Verdana";
-            ctx.fillStyle = "white";
-            ctx.fillText(palabra,0,130);
-        }
-    }*/
+    dibujarLetraIncorrecta(tecla)
 
+
+    console.log(tecla)
     //MUESTRA LA PALABRA ELEGIDA PARA PRUEBA
     //ctx.font="10pt Verdana";
     //ctx.fillStyle = "white";
@@ -139,21 +127,14 @@ function dibujarLetra(palabraAleatoria,teclaPresionada){
 
 
 //FUNCION: DIBUJAR LETRA INCORRECTA
-/*
-function dibujarLetraIncorrecta(teclaPresionada,palabras){
+
+function dibujarLetraIncorrecta(teclaPresionada){
 
     let tecla = teclaPresionada
-    let palabra =  palabras
-    if(tecla != palabra[i])
-    {
-        ctx.font="10pt Verdana";
-        ctx.fillStyle = "white";
-        ctx.fillText(tecla,0,145);
-    }
 
-    //MUESTRA LA PALABRA ELEGIDA PARA PRUEBA
-    //ctx.font="10pt Verdana";
-    //ctx.fillStyle = "white";
-    //ctx.fillText(palabra,-25,145,);
-    
-}*/
+    ctx.font="10pt Verdana";
+    ctx.fillStyle = "white";
+    ctx.fillText(tecla,0,145);
+
+}
+
